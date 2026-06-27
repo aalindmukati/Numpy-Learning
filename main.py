55be929df0973e342776c4a4e2434302a8d8133a
Aalind Mukati <aalindmukati@gmail.com>
2026-06-27 17:28:49 +0530

    Numpy Basic 1

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
 import numpy as np
+import time
+# print(np.__version__)
+
+n = 1_000_000 #this is the same way of saying 1million but the underscore acts like the comma 1,000,000
+
+py_list = list(range(n))
+np_array = np.arange(n)
+
+# print(py_list[0:5])
+# print(np_array[0:5])
+
+t1 = time.time()
+py_result=[x*2 for x in py_list]
+t2 = time.time()
+
+ttt = t2-t1
+
+t3 = time.time()
+py_result=np_array * 2
+t4 = time.time()
+
+tt = t4-t3
+a =[]
+
+for x in py_list:
+    a.append(x*2)
+
+print(len(a))
+print(a[:5])
+print(py_list[:5])
+
+print(f"numpy time {tt:.9f}")
+
+print(f"list time {ttt:.9f}")
