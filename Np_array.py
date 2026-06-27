81f2dae479dce1480e5d54d094f69b8020f1a4d9
Aalind Mukati <aalindmukati@gmail.com>
2026-06-27 17:46:18 +0530

    Np array creating

---
+++ Np_array.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+arr1 = np.array([1,2,3,4,5]) #1D array
+arr2 = np.array([[1,2,3],[4,5,6]]) #2d array
+arr3 = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[1,2,3]]]) #3d array
+
+
+
+print(f'{arr1}\n')
+print("-------")
+print(f'{arr2}\n')
+print("-------")
+
+print(f'{arr3}\n')
