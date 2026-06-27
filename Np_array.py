bbdc3e1b18fa09c3e71b7f25d1655fb3b05365f4
Aalind Mukati <aalindmukati@gmail.com>
2026-06-27 18:05:51 +0530

    Np array creating 2

--- Np_array.py
+++ Np_array.py
@@ -6,9 +6,14 @@ arr3 = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[1,2,3]]]) #3d array
 
 
 
-print(f'{arr1}\n')
-print("-------")
-print(f'{arr2}\n')
-print("-------")
+# print(f'{arr1}\n')
+# print("-------")
+# print(f'{arr2}\n')
+# print("-------")
 
-print(f'{arr3}\n')
+# print(f'{arr3}\n')
+
+print('shape=',arr2.shape)
+print('dimension=',arr2.ndim)
+print('data type=',arr3.dtype)
+print('total element=',arr1.size)
