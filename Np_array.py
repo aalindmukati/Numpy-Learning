222f500ed68e1b4d36e53efa0039fc3137f2cd84
Aalind Mukati <aalindmukati@gmail.com>
2026-06-28 12:20:00 +0530

    Np array creating 3

--- Np_array.py
+++ Np_array.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 arr1 = np.array([1,2,3,4,5]) #1D array
 arr2 = np.array([[1,2,3],[4,5,6]]) #2d array
@@ -13,10 +14,14 @@ arr3 = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[1,2,3]]]) #3d array
 
 # print(f'{arr3}\n')
 
-print('shape=',arr2.shape)
-print('dimension=',arr2.ndim)
-print('data type=',arr3.dtype)
-print('total element=',arr1.size)
+# print('shape=',arr2.shape)
+# print('dimension=',arr2.ndim)
+# print('data type=',arr3.dtype)
+# print('total element=',arr1.size)
 
 zeros = np.zeros([2,3])
 print(zeros)
+random_arr = np.random.rand(2,2)
+print(random_arr)
+eye = np.eye(3,3)
+print(eye)
