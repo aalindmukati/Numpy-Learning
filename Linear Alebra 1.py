736b0ba32c42b50242fe98f69dbbf1a2e4b75f4e
Aalind Mukati <aalindmukati@gmail.com>
2026-07-10 18:33:39 +0530

    Scalar ,vector and matrix

--- Linear Alebra 1.py
+++ Linear Alebra 1.py
@@ -1,1 +1,20 @@
 import numpy as np
+
+np.set_printoptions(suppress=True)
+
+scalar = 5.0
+print(scalar)
+print('type of scalar',type(scalar))
+
+vector = np.array(
+    [3,7,2]
+)
+print(vector)
+print("shape of vector",vector.shape)
+
+Matrix = np.array(
+    [[3,7,2],
+    [2,3,9]]
+)
+print(Matrix)
+print("shape of Matrix",Matrix.shape)
