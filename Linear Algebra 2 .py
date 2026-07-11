203a87a43dc025d2ee9dd1b27c1cd8eda6f000da
Aalind Mukati <aalindmukati@gmail.com>
2026-07-11 16:37:58 +0530

    t1245

---
+++ Linear Algebra 2 .py
@@ -0,0 +1,17 @@
+import numpy as np
+
+X =np.array(
+    [
+        [2,30],
+        [5,80],
+        [1,20]
+    ]
+)
+
+y = np.array([0,1,0])
+
+print ("Data matrix X: \n", X)
+print (" shape:", X.shape)
+
+print ("Output Vector y: \n", y)
+print("y shape:", y.shape) 
