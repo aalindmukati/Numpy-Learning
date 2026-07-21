c15ddde567a11f372922376b1edfc14bc9f8f7ed
Aalind Mukati <aalindmukati@gmail.com>
2026-07-21 19:25:24 +0530

    Euclidean Distance

--- Linear Algebra 4.py
+++ Linear Algebra 4.py
@@ -2,8 +2,13 @@ import numpy as np
 
 np.set_printoptions(suppress=True)
 
-v1=np.array((1,2))
-v2=np.array((4,3))
+# v1=np.array((1,2))
+# v2=np.array((4,3))
 
-print('v1=',v1)
-print('v2=',v2)
+# print('v1=',v1)
+# print('v2=',v2)
+
+# dot = np.dot(v1,v2)
+# print(dot)
+
+#Euclidean Distance
