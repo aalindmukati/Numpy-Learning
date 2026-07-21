d4b1afbb4adc863e9c3c51733af83e856c0a548b
Aalind Mukati <aalindmukati@gmail.com>
2026-07-21 19:40:54 +0530

    Euclidean Distance is just the distance between two vectors

--- Linear Algebra 4.py
+++ Linear Algebra 4.py
@@ -2,8 +2,8 @@ import numpy as np
 
 np.set_printoptions(suppress=True)
 
-# v1=np.array((1,2))
-# v2=np.array((4,3))
+v1=np.array((1,3))
+v2=np.array((4,2))
 
 # print('v1=',v1)
 # print('v2=',v2)
@@ -11,4 +11,12 @@ np.set_printoptions(suppress=True)
 # dot = np.dot(v1,v2)
 # print(dot)
 
-#Euclidean Distance
+#Euclidean Distance]
+
+diff = v1- v2
+
+print(diff)
+
+distance = np.linalg.norm(diff)
+
+print(distance)
