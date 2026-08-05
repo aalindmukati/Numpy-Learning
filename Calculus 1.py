e7ec31b621aa2e6e00e651814b5362005f916a0c
Aalind Mukati <aalindmukati@gmail.com>
2026-08-05 23:38:23 +0530

    Slope plotting

--- Calculus 1.py
+++ Calculus 1.py
@@ -1,1 +1,19 @@
 import numpy as np
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+np.set_printoptions(suppress=True)
+
+def line_function(x):
+    return 2 * x +1
+
+x = np.linspace(-3,3,100)
+y = line_function(x)
+
+print(x[:5])
+print(y[:5])
+
+plt.figure(figsize=(10,6))
+plt.plot(x,y)
+plt.show()
