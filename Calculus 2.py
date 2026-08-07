b07933e85d8dc635080bdf3b92d56427c5c67eb1
Aalind Mukati <aalindmukati@gmail.com>
2026-08-07 15:48:57 +0530

    Weight Upd documentation

--- Calculus 2.py
+++ Calculus 2.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 np.set_printoptions(suppress=True)
 
 x = np.array([1,2,3])
-y_true = np.array([6,2,4])
+y_true = np.array([3,2,4])
 
 # TODO f(x) = w * x
 # TODO y = w * x
 
-w = 1.0
+w = 3.0
 
 # Todo Learning Rate 
 lr = 0.01
@@ -26,7 +26,7 @@ def loss(w):
 
 def dloss(w):
     y_pred = w * x
-    return np.mean(2 * x * (y_pred - y_true))
+    return np.mean(2 * x * (y_pred - y_true)) # ! Slope = Average of [2× 𝑥 ×(guess−answer)]
 
 print("Start w:", w,"loss:",loss(w))
 
@@ -34,3 +34,8 @@ for step in range(30):
     slope = dloss(w)
     w = w-lr*slope
     print("Step", step + 1, "w:", round(w, 3),"loss: ",loss(w),3)
+
+# todo "Weight Updation"
+# = w - learning_rate * (dL/dw)**
+#If dL/dw is positive, increasing w increases loss,so we reduce w.
+#If dL/dw is negative,decreasing w increases loss, so we increase w. 
--- Linear Algebra 4.py
+++ Linear Algebra 4.py
@@ -11,7 +11,7 @@ v2=np.array((4,2))
 # dot = np.dot(v1,v2)
 # print(dot)
 
-#Euclidean Distance]
+# ! Euclidean Distance  {do vector k beech ki space ka distance}
 
 # diff = v1- v2
 
