41e43a533c0a72c19e3241b0dc22a6c6c1d1fafa
Aalind Mukati <aalindmukati@gmail.com>
2026-08-16 23:40:36 +0530

    plotting the probability for dice

--- Probability.py
+++ Probability.py
@@ -5,11 +5,21 @@ import pandas as pd
 
 np.random.seed(42)
 
-n = 500
-outcomes = np.random.choice(['H','T'],size = n) # so it creates an array with n elements in it and in that it can be either h or t
+n = 1000
+# outcomes = np.random.choice(['H','T'],size = n) # ! so it creates an array with n elements in it and in that it can be either h or t
 # print(outcomes[:150])
-heads = np.sum(outcomes='H')
-tails = np.sum(outcomes='T')
+# heads = np.sum(outcomes='H')
+# tails = np.sum(outcomes='T')
 
-print('Head:',heads)
-print('Tails:',tails)
+# print('Head:',heads)
+# print('Tails:',tails)
+
+dice = np.random.randint(1,7,size=n)
+p_dice = np.mean(dice == 4)
+print("Probability of 4 = ",p_dice)
+
+vals, counts = np.unique(dice,return_counts=True) #! YOU CAN PASS ANY ARRAY THROUGH IT AND WHAT THIS SAYS IS "FIND THE UNIQUE VALUES AND HOW MANY TIMES THEY HAVE APPEARED"
+print(vals)
+print(counts)
+sns.histplot(dice,discrete=True,kde=True)
+plt.show()
